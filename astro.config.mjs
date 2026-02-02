bf7896041a7afa0dea07a94d904ca2655b669f88
William <sart.william@gmail.com>
2026-02-02 10:52:56 +0100

    Fix astro import vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import tailwindcss from '@tailwindcss/vite';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://l-atelier-archi.vercel.app',
+
+  output: 'server',
 
   vite: {
     plugins: [tailwindcss()],
   },
 
-  output: 'server',
-
   integrations: [react(), keystatic(), markdoc()],
 
-  adapter: vercel(),
+  adapter: vercel({
+    runtime: 'nodejs',
+  }),
 });
