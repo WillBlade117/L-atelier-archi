79d91f51712b6e100c85f62d77f7a78574e69fd3
William <sart.william@gmail.com>
2026-02-03 15:21:53 +0100

    Activation auto deploy

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -48,26 +48,26 @@ export default config({
         coverImage: fields.image({
             label: 'Image de couverture (Grille)',
             directory: 'src/assets/projects', 
-            publicPath: '/assets/projects/',
+            publicPath: '../../assets/projects/',
         }),
 
         beforeImage: fields.image({
             label: 'Image AVANT (Slider)',
             directory: 'src/assets/projects', 
-            publicPath: '/assets/projects/',
+            publicPath: '../../assets/projects/',
         }),
 
         afterImage: fields.image({
             label: 'Image APRÈS (Slider)',
             directory: 'src/assets/projects', 
-            publicPath: '/assets/projects/',
+            publicPath: '../../assets/projects/',
         }),
 
         gallery: fields.array(
             fields.image({
                 label: 'Image',
                 directory: 'src/assets/projects',
-                publicPath: '/assets/projects/',
+                publicPath: '../../assets/projects/',
             }),
             {
                 label: 'Galerie Photos',
@@ -82,7 +82,7 @@ export default config({
             links: true,
             images: {
                 directory: 'src/assets/projects',
-                publicPath: '/assets/projects/',
+                publicPath: '../../assets/projects/',
             },
         }),
       },
